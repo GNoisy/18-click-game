867ab95412e4c3372e93e74bcdd44aaad30493aa
Giovanni Noisy <gnoisy77@gmail.com>
2020-08-09 22:45:32 -0700

    reworked navbar layout

--- clickgame/src/components/Navbar.js
+++ clickgame/src/components/Navbar.js
@@ -3,15 +3,17 @@ import "../styles/Navbar.css";
 
 const styles = {
     navbarStyle: {
-      background: "green",
-    //   justifyContent: "flex-end"//flex-end: items are packed toward to end line https://css-tricks.com/almanac/properties/j/justify-content/
+      // background: "green",
+      justifyContent: "space-between"//flex-end: items are packed toward to end line https://css-tricks.com/almanac/properties/j/justify-content/
     }
 };
 
 function Navbar() {
     return (
-      <nav style={styles.navbarStyle} className="navbar">
-        <a href="/">Welcome</a>
+
+      <nav style={styles.navbarStyle} className="navbar sticky-top">
+        <a className="navbar-brand" href="#"><span><h2>Bob's Burger</h2></span></a>
+        <h2 className="text-white">Score: <span>0</span></h2>
       </nav>
     );
 }
