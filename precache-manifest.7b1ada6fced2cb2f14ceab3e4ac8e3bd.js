0be790736ee36c8cb00835d4ff5663a49f2f50f9
Giovanni Noisy <gnoisy77@gmail.com>
2020-08-11 22:53:57 -0700

    Updates

---
+++ precache-manifest.7b1ada6fced2cb2f14ceab3e4ac8e3bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed09acd68688bc420d6a72f5430485a1",
+    "url": "/18-click-game/index.html"
+  },
+  {
+    "revision": "7eabc7c0250bccd9fc34",
+    "url": "/18-click-game/static/css/main.245f2a3c.chunk.css"
+  },
+  {
+    "revision": "29d40325a51744729faa",
+    "url": "/18-click-game/static/js/2.d8ee25d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/18-click-game/static/js/2.d8ee25d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eabc7c0250bccd9fc34",
+    "url": "/18-click-game/static/js/main.0899c06d.chunk.js"
+  },
+  {
+    "revision": "82a22a4cd940a1675397",
+    "url": "/18-click-game/static/js/runtime-main.c2430685.js"
+  }
+]);
