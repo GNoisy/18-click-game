9d91a9671aa876a4194961cace4ca4d525cf765f
Giovanni Noisy <gnoisy77@gmail.com>
2020-08-27 17:27:34 -0700

    deleted commented out code

--- clickgame/src/App.js
+++ clickgame/src/App.js
@@ -6,53 +6,6 @@ import images from './images.json';
 import Wrapper from "./components/Wrapper";
 
 
-// function App() {
-//   return (
-//     <div>
-//       <Navbar />
-//       <Jumbotron />
-//       <Wrapper>
-//         <CharCard 
-//         image={images[0].image}
-//         />
-//         <CharCard 
-//         image={images[1].image}
-//         />
-//         <CharCard 
-//         image={images[2].image}
-//         />
-//         <CharCard 
-//         image={images[3].image}
-//         />
-//         <CharCard 
-//         image={images[4].image}
-//         />
-//         <CharCard 
-//         image={images[5].image}
-//         />
-//         <CharCard 
-//         image={images[6].image}
-//         />
-//         <CharCard 
-//         image={images[7].image}
-//         />
-//         <CharCard 
-//         image={images[8].image}
-//         />
-//         <CharCard 
-//         image={images[9].image}
-//         />
-//         <CharCard 
-//         image={images[10].image}
-//         />
-//         <CharCard 
-//         image={images[11].image}
-//         />
-//       </Wrapper>
-//     </div>
-//   );
-// }
-
 class App extends React.Component {
   state = {
     images: images,
